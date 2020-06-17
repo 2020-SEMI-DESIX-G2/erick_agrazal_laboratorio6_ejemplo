efd1daf4803880c8efc0b251299c42dcafe0e351
Erick Agrazal <erick.agrazal@gmail.com>
2020-06-17 07:17:01 -0500

    Added db configuration and initial estudiantes schemas.

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 require('dotenv').config()
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const bodyParser = require('body-parser')
 
-const settings = {
-  PORT: process.env.PORT || 3000,
-};
+const connectDb = require('./dbConfig');
+const Estudiantes = require('./models/Estudiantes');
 
-const ESTUDIANTES = [
-  {
-    nombre: "Erick Agrazal",
-    edad: 29,
-  },
-  {
-    nombre: "Jusselly Moreno",
-    edad: 26,
-  }
-]
+const PORT = 3000;
+
 
 // Intermediarios
 app.use(bodyParser.json());
 
+
 // Controladores
-app.get('/api/estudiantes/', function (req, res) {
-  res.json({
-    cantidad: ESTUDIANTES.length,
-    estudiantes: ESTUDIANTES
-  });
+app.get('/api/estudiantes/', async (req, res) => {
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+    res.json({
+        estudiantes,
+        cantidad: estudiantes.length
+    });
+});
+app.post('/api/estudiantes/', async (req, res) => {
+    const { nombre, edad } = req.body;
+    await Estudiantes.create({ nombre, edad });
+    res.json({ nombre, edad });
+});
+app.get('/api/estudiantes/:id', async (req, res) => {
+    try {
+        const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
+        res.json(estudiante);
+    } catch (error) {
+        console.log(error);
+        res.json({});
+    }
 });
 
-app.get('/api/estudiantes/:id', function (req, res) {
-  res.json(ESTUDIANTES[req.params.id]);
-});
-
-app.post('/api/estudiantes/', function (req, res) {
-  ESTUDIANTES.push(req.body);
-  res.json(req.body);
-});
-
-app.listen(settings.PORT, () => {
-  console.log(`El servidor está ejecutando en el puerto ${settings.PORT}`);
+connectDb().then(() => {
+    app.listen(PORT, () => {
+      console.log(`Ejecutando en el puerto ${PORT}`);
+    });
 });
